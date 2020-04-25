d41d86350d77542cf36a39601b9d7272643149c5
hamidsultanzadeh <sultanzade.06@mail.ru>
2020-04-25 13:54:05 +0400

    print, input, calculator

---
+++ calculator-without-if.py
@@ -0,0 +1,7 @@
+number1 = float(input("Number 1 : "))
+number2 = float(input("Number 2 : "))
+
+print(number1+number2)
+print(number1*number2)
+print(number1/number2)
+print(number1-number2)
---
+++ print-function.py
@@ -0,0 +1,22 @@
+# x = 10
+# print(x * "hello\n")
+
+# print("Hi", "Guys", "How are you?!", sep="\n", end="!")
+
+name = "Hamid"
+surname = "Sultanzadeh"
+date = "21-09-2000"
+placeBirth = "Baku, Azerbaijan"
+phone = "+994 51 318 07 07"
+job = "Java Developer"
+workAt = "BSP Tech"
+
+print("==============================")
+print("* Name     :", name)
+print("* Surname  :", surname)
+print("* Birthday :", date)
+print("* Place    :", placeBirth)
+print("* Phone    :", phone)
+print("* Job      :", job)
+print("* Work at  :", workAt)
+print("==============================")
