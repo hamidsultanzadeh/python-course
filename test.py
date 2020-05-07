40f120caadafbf387cd13f329ec42d419a728127
hamidsultanzadeh <sultanzade.06@mail.ru>
2020-05-07 18:32:42 +0400

    test.py

---
+++ test.py
@@ -0,0 +1,26 @@
+from decimal import Decimal
+
+
+# https://stackoverflow.com/questions/48328441/python-decimal-as-integer-ratio-output
+
+eded = Decimal('9.2')
+eded2 = Decimal('9.2')
+
+# print(eded.is_integer())
+
+print(46/5)
+
+for i in range(10):
+
+    my_set = eded2.as_integer_ratio()
+
+    print(my_set)
+
+my_list = [1,2,3,4,5]
+
+print(sum(my_list,1))
+
+"""
+salam
+
+"""
